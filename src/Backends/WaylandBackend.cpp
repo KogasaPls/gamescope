8d60468a5647bd6e89eed0abc8724bb97aa060f1
kogasa <kogasapls@gmail.com>
2026-09-03 22:21:29 -0400

    steamcompmgr: drive the nested output's HDR from content without a remake
    The Wayland connector opts into upstream's content-driven HDR. An HDR flip
    on a backend without a Vulkan swapchain no longer remakes the output
    images, since nothing about them depends on the flag, but it still forces
    a frame, since the transfer function otherwise changes with no frame
    queued.

--- src/Backends/WaylandBackend.cpp
+++ src/Backends/WaylandBackend.cpp
@@ -994,6 +994,7 @@ namespace gamescope
         , m_Planes{ this, this, this, this, this, this, this, this }
     {
         m_HDRInfo.bAlwaysPatchEdid = true;
+        m_HDRInfo.bContentDrivenHDR = true;
     }
 
     CWaylandConnector::~CWaylandConnector()
